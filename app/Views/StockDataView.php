8a6613d9f6a006d27977b755bbe612f99e13349b
Ieva Kalnina <ieakalnina@gmail.com>
2020-11-06 00:32:08 +0200

    Update StockDataView.php

--- app/Views/StockDataView.php
+++ app/Views/StockDataView.php
@@ -1,12 +1,44 @@
 <html>
-    <h1>Stock <?php echo $id; ?></h1>
+<head>
+    <style>
+        span {
+            display: inline-block;
+            width: 200px;
+        }
+    </style>
+</head>
+<a href="/">Back</a>
+<h1>Stock <?php echo $id; ?></h1>
 <ul>
-    <li>Previous Close <?php echo $stock->regularMarketPreviousClose();?></li>
-    <li>Open <?php echo $stock->regularMarketOpen();?></li>
-    <li>Bid <?php echo $stock->bid() . 'x' . $stock->bidSize();?></li>
-    <li>Ask <?php echo $stock->ask() . 'x' . $stock->askSize();?></li>
-    <li>Day's Range <?php echo $stock->regularMarketDayLow() . '-' . $stock->regularMarketDayHigh();?></li>
-    <li>52 Week Range <?php echo $stock->fiftyTwoWeekLow() . '-' . $stock->fiftyTwoWeekHigh();?></li>
-    <li>Volume <?php echo $stock->regularMarketVolume();?></li>
+    <li>
+        <span>Previous Close </span>
+        <span><?php echo number_format($stock->regularMarketPreviousClose(), 2); ?></span>
+    </li>
+    <li>
+        <span>Open </span>
+        <span><?php echo number_format($stock->regularMarketOpen(), 2); ?></span>
+    </li>
+    <li>
+        <span>Bid </span>
+        <span><?php echo number_format($stock->bid(), 2) . ' x ' . $stock->bidSize() * 100; ?></span>
+    </li>
+    <li>
+        <span>Ask </span>
+        <span><?php echo number_format($stock->ask(), 2) . ' x ' . $stock->askSize() * 100; ?></span>
+    </li>
+    <li>
+        <span>Day's Range </span>
+        <span><?php echo number_format($stock->regularMarketDayLow(), 2);
+            echo ' - ' . number_format($stock->regularMarketDayHigh(), 2); ?></span>
+    </li>
+    <li>
+        <span>52 Week Range </span>
+        <span><?php echo number_format($stock->fiftyTwoWeekLow(), 2);
+        echo ' - ' . number_format($stock->fiftyTwoWeekHigh(), 2); ?></span>
+    </li>
+    <li>
+        <span>Volume </span>
+        <span><?php echo number_format($stock->regularMarketVolume()); ?></span>
+    </li>
 </ul>
 </html>
